b65fc6bde05ecb0ba7280316229afaae8b2e1f67
egor5q <31898567+egor5q@users.noreply.github.com>
2017-11-30 19:58:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,23 @@ def chlenomer(message):
 
 @bot.message_handler(commands=['chlen'])
 def chlen2(message):
-    chlenomer(message)
+    if message.text in massive:
+        print(message.chat.id)
+        chlen=random.randint(1,100)
+        mm=random.randint(0,9)
+        randomvoice=random.randint(1,100)
+        if randomvoice>90:
+              chlen = random.randint(1, 2)
+              if chlen == 1:
+                  text = 'Как у коня'
+              elif chlen==2:
+                  text='5000км! Мужик!'
+              bot.send_message(message.chat.id, 'Размер члена ' + message.from_user.first_name + ': ' + text)
+
+        else:
+            replytext='Размер члена '+message.from_user.first_name+': '+str(chlen)+','+str(mm)+' см'
+            bot.send_message(message.chat.id, replytext)
+    
     
 
 
