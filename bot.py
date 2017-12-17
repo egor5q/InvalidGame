21707b49fb5adcbc898f4f94f6e03085f5e6ecf5
egor5q <31898567+egor5q@users.noreply.github.com>
2017-12-17 19:19:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,9 +10,30 @@ token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(token)
 
 
-@bot.message_handler(commands=['startgame'])
-def startgame(m):
-    if message.chat.id not in info.lobby.game:
+@bot.message_handler(commands=['solo'])
+def solo(m):
+    if m.chat.id not in info.lobby.game:
+      if m.chat.id<0:
+        game=creategame(m.chat.id)
+        info.lobby.game.update(game)
+        bot.send_message(m.chat.id, 'Вы начали игру в режиме "Каждый за себя"! Ожидайте игроков')
+      else:
+        bot.send_message(m.chat.id, 'Играть в этот режим можно только в группах!')
+    else:
+        bot.send_message(m.chat.id, 'Игра уже была запущена в этом чате!')
+        
+        
+@bot.message_handler(commands=['join'])
+def join(m):
+    if m.chat.id in info.lobby.game:
+        if m.from_user.id not in info.lobby.game[m.chat.id][players]:
+          try:
+            bot.send_message(m.from_user.id, 'Вы успешно присоединились!')
+            info.lobby.game[m.chat.id][players].append(m.from_user.id)
+            bot.send_message(m.chat.id, 'Вы успешно присоединились! Количество игроков: '+str(len(info.lobby.game[m.chat.id][players])))
+          except:
+            bot.send_message(m.chat.id, 'Сначала напишите боту @customwarbot !')
+
         
 
 
@@ -21,7 +42,13 @@ def startgame(m):
     
 
         
-                         
+def creategame(id):
+    return id:{
+        chatid:id,
+        players:[]
+
+    }
+               
 
 
 
