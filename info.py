7b37704cb355ac3a8d362cd1069addf1a312f60e
egor5q <31898567+egor5q@users.noreply.github.com>
2017-12-18 16:57:48 +0300

    Update info.py

--- info.py
+++ info.py
@@ -6,9 +6,28 @@ class Lobby(object):
 class Ninja(object):
     def __init__(self):
         self.hp=20
-        self.damage=5
+        self.damage=4
+        self.evade=15
+        self.accuracy=50
         
         
+class Robot(object):
+    def __init__(self):
+        self.hp=30
+        self.damage=3
+        self.evade=2
+        self.accuracy=60
+        
+        
+class Berserk(object):
+    def __init__(self):
+        self.hp=25
+        self.damage=6
+        self.evade=5
+        self.accuracy=40
+        
+berserk=Berserk()        
+robot=Robot()        
 ninja=Ninja()        
 lobby = Lobby()
    
