b7abdb0d3909bb1ea670cd2883b80327c4bba068
egor5q <31898567+egor5q@users.noreply.github.com>
2017-12-18 16:36:31 +0300

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,12 @@ class Lobby(object):
         self.game={}
         
         
+class Ninja(object):
+    def __init__(self):
+        self.hp=20
+        self.damage=5
         
+        
+ninja=Ninja()        
 lobby = Lobby()
    
