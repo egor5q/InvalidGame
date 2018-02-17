2d2d9f0751a3d042c68633e29edb1ca79c34b07f
egor5q <31898567+egor5q@users.noreply.github.com>
2018-02-17 14:23:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,6 +33,7 @@ def inline(call):
         Keyboard.add(types.InlineKeyboardButton(text='АК-47', callback_data='ak')) 
         Keyboard.add(types.InlineKeyboardButton(text='Рандомно', callback_data='random')) 
         msg=bot.send_message(call.from_user.id, 'Теперь выберите оружие каждому. Выбор для: '+info.lobby.game[call.from_user.id]['bots'][x]['name'])
+        x+=1
       
   
   elif call.data=='rock':
