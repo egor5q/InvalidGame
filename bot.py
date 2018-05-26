628b357000a8b8788f5ea7bc136be6b5e4cd4c55
egor5q <31898567+egor5q@users.noreply.github.com>
2018-05-26 12:29:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -215,6 +215,7 @@ def inline(call):
   elif call.data=='back':
        kb=types.InlineKeyboardMarkup()
        kb.add(types.InlineKeyboardButton(text='ХП', callback_data='hp'), types.InlineKeyboardButton(text='Урон', callback_data='dmg'),types.InlineKeyboardButton(text='Прочее', callback_data='different'))
+       kb.add(types.InlineKeyboardButton(text='Закрыть меню', callback_data='close'))
        medit('Выберите ветку',call.message.chat.id, call.message.message_id, reply_markup=kb)
        
   elif call.data=='zombie':
