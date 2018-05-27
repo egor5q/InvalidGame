c13ee833e94a23827b2ea160ed44e3bd9913543c
egor5q <31898567+egor5q@users.noreply.github.com>
2018-05-27 11:39:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -151,11 +151,11 @@ def k(m):
         pass
         
 
-@bot.message_handler(commands=['update'])
-def upd(m):
-        if m.from_user.id==441399484:
-                 users.update_many({}, {'$set':{'bot.gipnoz':0}})
-                 print('yes')
+#@bot.message_handler(commands=['update'])
+#def upd(m):
+#        if m.from_user.id==441399484:
+#                 users.update_many({}, {'$set':{'bot.gipnoz':0}})
+#                 print('yes')
                 
 
 @bot.message_handler(commands=['buybox'])
