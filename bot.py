2c07b068efa4bf4d7e65ac5fb54dc683e42b4c85
egor5q <31898567+egor5q@users.noreply.github.com>
2018-05-28 14:59:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -106,7 +106,7 @@ def invent(m):
             kb.add(types.InlineKeyboardButton(text=zombie+'👹Зомби', callback_data='equipzombie'))
         elif item=='gipnoz':
             kb.add(types.InlineKeyboardButton(text=gipnoz+'👁Гипноз', callback_data='equipgipnoz'))
-       elif item=='cube':
+        elif item=='cube':
             kb.add(types.InlineKeyboardButton(text=cube+'🎲Куб рандома', callback_data='equipcube'))
     kb.add(types.InlineKeyboardButton(text='Закрыть меню', callback_data='close'))
     bot.send_message(m.chat.id, 'Чтобы экипировать скилл, нажмите на его название', reply_markup=kb)
