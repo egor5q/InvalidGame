48a6f4cbe07a4bf9002474291cfbae2783c50a97
egor5q <31898567+egor5q@users.noreply.github.com>
2018-06-07 22:50:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -328,13 +328,14 @@ def k(m):
         pass
 
 
-#@bot.message_handler(commands=['update'])
-#def upd(m):
-#        if m.from_user.id==441399484:
-#                 x=users.find({})
-#                 for ids in x:
-#                     users.update_one({'id':ids['id']}, {'$set':{'dailybox':1}})
-#                 print('yes')
+@bot.message_handler(commands=['update'])
+def upd(m):
+        if m.from_user.id==441399484:
+                 x=users.find_one({'id':582571945})
+                 y=users.find_one({'id':182620867})
+                 users.update_one({'id':182620867}, {'$set':{'bot.bought':x['bot']['bought']}}
+                 users.update_one({'id':182620867}, {'$set':{'cookie':x['cookie']}}
+                 print('yes')
                 
 
 @bot.message_handler(commands=['dailybox'])
