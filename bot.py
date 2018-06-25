9a9ea07af036165e2b8bf62fdb71555b1c0cead7
egor5q <31898567+egor5q@users.noreply.github.com>
2018-06-25 22:54:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,7 +74,7 @@ items=['flash', 'knife']
 @bot.message_handler(commands=['update'])
 def upd(m):
         if m.from_user.id==441399484:
-            users.insert_one({createboss(0})
+            users.insert_one({createboss(0)})
             print('yes')
             
             
