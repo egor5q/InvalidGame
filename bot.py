3b1ed2e257e2687527a6b6b1544816dac6da2d47
egor5q <31898567+egor5q@users.noreply.github.com>
2018-10-10 18:08:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2926,8 +2926,7 @@ def medit(message_text,chat_id, message_id,reply_markup=None,parse_mode='Markdow
     return bot.edit_message_text(chat_id=chat_id,message_id=message_id,text=message_text,reply_markup=reply_markup,
                                  parse_mode=parse_mode)        
         
-
-modetoname(x):
+def modetoname(x):
    if x=='meteors':
       return 'Метеоритный дождь'
    if x=='randomhp':
