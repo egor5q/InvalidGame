f88b77588cf2e7c615849b587946486facb4de17
egor5q <31898567+egor5q@users.noreply.github.com>
2018-10-24 00:30:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -810,7 +810,7 @@ def inline(call):
         kb.add(types.InlineKeyboardButton(text=mage+'✨Колдун', callback_data='mage'))
         kb.add(types.InlineKeyboardButton(text=mage+'🔥Повелитель огня', callback_data='firemage'))
         kb.add(types.InlineKeyboardButton(text=necromant+'🖤Некромант', callback_data='necromant'))
-        kb.add(types.InlineKeyboardButton(text=bloodmage+'🔵Магический титан', callback_data='magictitan'))
+        kb.add(types.InlineKeyboardButton(text=magictitan+'🔵Магический титан', callback_data='magictitan'))
         medit('Ветка: вампиризм', call.message.chat.id, call.message.message_id, reply_markup=kb)
        
   elif call.data=='shieldgen':
