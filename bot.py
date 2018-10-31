a15078c6951ca827056428d56460ab3a54e5416e
egor5q <31898567+egor5q@users.noreply.github.com>
2018-10-31 23:33:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3688,7 +3688,7 @@ def foo(bar):
         except:
            pass
       if z!=None and id!=None:
-         users.update_one({'id':int(id)},{'$inc':{'cookie':bar[ids]['price']*20}})
+         users.update_one({'id':int(id)},{'$inc':{'cookie':int(bar[ids]['price']*20)}})
          bot.send_message(int(id),'Ваш платёж прошёл успешно! Получено: '+str(bar[ids]['price']*20)+'⚛')
          donates.update_one({},{'$pull':{'donaters':id}})      
       bot.send_message(441399484,'New payment!')
